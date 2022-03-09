ebb6363ce3f1b16073cb0f017e4aed241e9c7056
K <70395083+kxtsn@users.noreply.github.com>
2022-03-09 11:40:38 +0800

    Backend + DB

---
+++ Code/Backend/routes/api/auth/index.js
@@ -0,0 +1,21 @@
+var router = require('express').Router();
+
+console.log("In api/auth")
+
+router.use('/login', require('./login'));
+
+router.use(function(err, req, res, next){
+  if(err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key){
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err);
+});
+
+module.exports = router;
