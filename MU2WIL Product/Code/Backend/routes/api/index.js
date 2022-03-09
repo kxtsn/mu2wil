62e416b9113a3809d24bae6f2439e12404aa0222
K <70395083+kxtsn@users.noreply.github.com>
2022-03-09 11:35:58 +0800

    Backend
    Backend base and DB Version 1 Dump

---
+++ MU2WIL Product/Code/Backend/routes/api/index.js
@@ -0,0 +1,9 @@
+var router = require('express').Router();
+
+router.use('/auth', require('./auth'));
+router.use('/super-admin', require('./super-admin'));
+router.use('/portal-manager', require('./portal-manager'));
+router.use('/student', require('./student'));
+router.use('/employer', require('./employer'));
+
+module.exports = router;
