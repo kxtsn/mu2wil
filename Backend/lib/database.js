b1b5dd6de41c5d397e8dfbfacbd956088eb688d4
K <70395083+kxtsn@users.noreply.github.com>
2022-03-09 11:38:00 +0800

    Backend Main

---
+++ Backend/lib/database.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const util = require("util"); 
+
+const pool  = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mu2wil',
+  insecureAuth: true,
+  timezone: 'Z',
+  supportBigNumbers: true,
+}); 
+
+const connection = () => {
+  return new Promise((resolve, reject) => {
+  pool.getConnection((err, connection) => {
+    if (err) reject(err);
+    console.log("MySQL pool connected: threadId " + connection.threadId);
+    const query = (sql, binding) => {
+      return new Promise((resolve, reject) => {
+         connection.query(sql, binding, (err, result) => {
+           if (err) reject(err);
+           resolve(result);
+           });
+         });
+       };
+       const release = () => {
+         return new Promise((resolve, reject) => {
+           if (err) reject(err);
+           console.log("MySQL pool released: threadId " + connection.threadId);
+           resolve(connection.release());
+         });
+       };
+       resolve({ query, release });
+     });
+   });
+ };
+const query = (sql, binding) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, binding, (err, result, fields) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+module.exports = { pool, connection, query };
